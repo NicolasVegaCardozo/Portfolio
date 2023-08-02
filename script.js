650464907468a7597ad0afdf53cd1b135485ca58
Your Name~ <you@example.com>
2023-08-02 11:31:10 -0300

    Third commit

--- script.js
+++ script.js
@@ -15,3 +15,17 @@ const seleccionar = () => {
     menuVisible = false;
 }
 
+const efectoHabilidades = () => {
+    let skills = document.getElementById("skills");
+    let distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if(distancia_skills >= 300){
+        let habilidades = document.getElementsByClassName("progreso");
+        habilidades[0].classList.add("javascript");
+        habilidades[1].classList.add("htmlcss");
+        habilidades[2].classList.add("react");
+        habilidades[3].classList.add("comunicacion");
+        habilidades[4].classList.add("trabajoEnEquipo");
+        habilidades[5].classList.add("creatividad");
+        habilidades[6].classList.add("dedicacion");
+    }
+}
